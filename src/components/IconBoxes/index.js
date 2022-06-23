ed0737c5ead4ab60b001ba3e0beb8bc6e22cc5d9
edk21 <edwinmcken@gmail.com>
2022-06-23 19:34:17 +0200

    fade effect added to the icon-boxes

--- src/components/IconBoxes/index.js
+++ src/components/IconBoxes/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Container, Row, Col} from "react-bootstrap"
 import { FaBasketballBall, FaFileAlt, FaTachometerAlt, FaLayerGroup } from "react-icons/fa"
 import { iconboxItems } from "./iconBoxItems"
+import Fade from "react-reveal/Fade"
 
 const IconBoxes = () => {
   return (
@@ -10,20 +11,24 @@ const IconBoxes = () => {
             <Row>
                 {
                     iconboxItems.map(item => (
+                        
                         <Col key={item.id} md={6} lg={3} className="d-flex align-items-stretch mb-5 mb-lg-0">
-                            <div className="icon__box">
-                                <div className="icon">
-                                    {
-                                        item.id === 1 ? <FaBasketballBall /> :
-                                        item.id === 2 ? <FaFileAlt /> :
-                                        item.id === 3 ? <FaTachometerAlt /> :
-                                        item.id === 4 ? <FaLayerGroup /> : null
-                                    }
+                            <Fade up>
+                                <div className="icon__box">
+                                    <div className="icon">
+                                        {
+                                            item.id === 1 ? <FaBasketballBall /> :
+                                            item.id === 2 ? <FaFileAlt /> :
+                                            item.id === 3 ? <FaTachometerAlt /> :
+                                            item.id === 4 ? <FaLayerGroup /> : null
+                                        }
+                                    </div>
+                                    <h4 className="title">{item.title}</h4>
+                                    <p className="description">{item.text}</p>
                                 </div>
-                                <h4 className="title">{item.title}</h4>
-                                <p className="description">{item.text}</p>
-                            </div>
+                            </Fade>
                         </Col>
+                        
                     ))
                 }
             </Row>
